6de02e65df11912a1b78edcd8e1ac43eae12d95f
Jesse Karnik <jessek7@uw.edu>
2024-05-06 16:22:33 -0700

    Adding map marker feature

---
+++ src/Model.js
@@ -0,0 +1,52 @@
+'use strict';
+let map;
+//array of markers position
+let markers = [];
+// Define the initMap function
+export async function initMap() {
+  // Set location at Seattle
+  const position = { lat: 47.608013, lng: -122.335167 };
+
+  // Create a map element
+  const mapElement = document.createElement("div");
+  mapElement.setAttribute("id", "mapElement");
+  // Set intial size of map
+  mapElement.style.height = "400px";
+  mapElement.style.width = "100%";
+  // Import needed library
+  const { Map } =await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // Append the map element to the body
+  let main = document.querySelector('#mapMain');
+  main.appendChild(mapElement);
+
+  // Render the map
+  map = new Map(mapElement, {
+      zoom: 12,
+      center: position,
+      mapId: 'Seattle'
+  });
+
+  return map;
+}
+//add new marker on google map, takes a position in the format
+//{lat:x,lng:y} and name of the marker. 
+//Probably new more modification but will let it be now. 
+export function addMarker(newPosition,name){
+  google.maps.importLibrary("marker").then(({ AdvancedMarkerElement }) => {
+    // Add new position to markers array
+    markers.push(newPosition);
+    // Clear existing markers and add new ones
+    if(markers&&markers.length!==0){
+      markers.forEach(mark =>{
+        const marker = new AdvancedMarkerElement({
+          position: mark,
+          map: map,
+          title: name,
+      })
+      })
+    }
+  })
+}
+
---
+++ src/testfile.js
@@ -0,0 +1,8 @@
+'use strict';
+import * as module from './Model.js';
+module.initMap();
+module.addMarker({ lat: 47, lng: -122 },'name');
+module.addMarker({ lat: 48, lng: -123},'name2');
+module.addMarker({ lat: 48.5, lng: -123.5},'name3');
+
+
