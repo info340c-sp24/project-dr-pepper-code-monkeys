e81f3bb0e37ff445382ed79ae883d996e3949505
zowee101 <zaquino@uw.edu>
2024-06-03 20:23:35 -0700

    fixed footer and header in mobile

--- src/Footer.js
+++ src/Footer.js
@@ -3,27 +3,25 @@ import './index.css';
 
 export function Footer() {
     return (
-        <footer className='pt-3 border-dark border-top border-1' >
-            <div className='d-flex flex-row wrap justify-content-between'>
-                    <div className='mx-3 d-flex justify-content-start'>
-                        <img src="/img/logo.svg" alt="FFS logo" className="rounded img-fluid w-25 h-25 d-inline" />
-                        <p className='fs-4 text-secondary'> Find Food Seattle<span className="copyright"> &copy;2024</span></p>
-                    </div>
-                    <div className='mx-3 d-flex'>
-                        <div className="me-3">
-                            <h3>Contact Us</h3>
-                            <div id="social-links">
-                                <a href="#" className="fa fa-instagram mx-1 link-underline link-underline-opacity-0"></a>
-                                <a href="#" className="fa fa-twitter-square mx-1 link-underline link-underline-opacity-0" aria-label="twitter"></a>
-                                <a href="#" className="fa fa-facebook-square mx-1 link-underline link-underline-opacity-0" aria-label="facebook"></a>
-                                <a href="#" className="fa fa-github mx-1 link-underline link-underline-opacity-0" aria-label="github"></a>
-                            </div>
+        <footer className='footer pt-3 border-dark border-top border-1'>
+            <div className='footer-content d-flex flex-column flex-md-row justify-content-between align-items-center'>
+                <div className='footer-left d-flex flex-column align-items-center'>
+                    <img src="/img/logo.svg" alt="FFS logo" className="rounded img-fluid logo-footer" />
+                    <p className='fs-4 text-secondary text-center'> Find Food Seattle<span className="copyright"> &copy;2024</span></p>
+                </div>
+                <div className='footer-right d-flex flex-column align-items-center'>
+                    <div className="contact-info text-center">
+                        <h3>Contact Us</h3>
+                        <div id="social-links" className="mb-2">
+                            <a href="https://www.instagram.com" className="fa fa-instagram mx-1 link-underline link-underline-opacity-0" aria-label="Instagram"></a>
+                            <a href="https://twitter.com" className="fa fa-twitter-square mx-1 link-underline link-underline-opacity-0" aria-label="Twitter"></a>
+                            <a href="https://www.facebook.com" className="fa fa-facebook-square mx-1 link-underline link-underline-opacity-0" aria-label="Facebook"></a>
+                            <a href="https://github.com" className="fa fa-github mx-1 link-underline link-underline-opacity-0" aria-label="GitHub"></a>
                         </div>
-                        <div>
-                            <p><a className='link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href='tel: +18880000000'>Phone: (206) 785-9341</a></p>
-                            <p><a className='link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href='mailto:example@uw.edu'>Email: drpeppercodingmonkeys@uw.edu</a></p>
-                        </div>
+                        <p className='footer-contact'><a className='link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href='tel:+18880000000'>Phone: (206) 785-9341</a></p>
+                        <p className='footer-email'><a className='link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href='mailto:drpeppercodingmonkeys@uw.edu'>Email: drpeppercodingmonkeys@uw.edu</a></p>
                     </div>
+                </div>
             </div>
         </footer>
     );
